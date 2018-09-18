df6bdf22d2fb7ff387116d194491c4da2d4249a5
Michael Schwarz <michael_schwarz@me.com>
2018-09-18 18:43:08 +0200

    Initial commit

--- Package.swift
+++ Package.swift
@@ -4,14 +4,10 @@ import PackageDescription
 let package = Package(
     name: "Marvin",
     dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🍃 An expressive, performant, and extensible templating language built for Swift.
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Leaf", "Vapor"]),
+        .target(name: "App", dependencies: ["Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
