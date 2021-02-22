dd46e7d916bc35dc403cd74dc20df6de8ba8b285
Michael Schwarz <michael_schwarz@me.com>
2021-02-22 22:07:01 +0100

    Update package to use vapor 4

--- Package.swift
+++ Package.swift
@@ -1,16 +1,21 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "Marvin",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .library(name: "Marvin", targets: ["Marvin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
     ],
     targets: [
-        .target(name: "Marvin", dependencies: ["Vapor"]),
+        .target(name: "Marvin", dependencies: [
+            .product(name: "Vapor", package: "vapor"),
+        ]),
         .target(name: "Dev", dependencies: ["Marvin"]),
         .testTarget(name: "AppTests", dependencies: ["Marvin"])
     ]
